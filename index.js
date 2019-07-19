dcf1dda3fe18d16e2853a8fb6dc652198bd447e3
Alex Cabo Martinez <alex@MacBook-Air-de-Alex.local>
2019-07-19 10:43:07 +0200

    somestyle

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ window.onload = () => {
     let start = document.getElementById("start")
     let audioTheme = document.getElementById("theme")
     let restartGame = document.getElementById("restart-button")
-    let restartOut = document.getElementById("restart")
+    let controlsOut = document.getElementById("controls")
 
     start.onclick = () => {
 
         start.className = "out"
+        controlsOut.className = "out"
         Game.init()
         audioTheme.play()
     }
 
 
-    restartGame.onclick = () => {
+    // restartGame.onclick = () => {
 
-        restartOut.className = "res"
-        Game.init()
-        audioTheme.play()
-    }
+    //     restartOut.className = "res"
+    //     Game.init()
+    //     audioTheme.play()
+    // }
 }
